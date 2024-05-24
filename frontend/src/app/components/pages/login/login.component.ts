ee6b6e3719c398bfa7d3dfa7a2c5a0e0ec6ac985
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2024-05-23 21:54:40 -0300

    primeiros arquivos

---
+++ frontend/src/app/components/pages/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(event: Event): void {
+    event.preventDefault(); // Evita o comportamento padrão de envio do formulário
+    if (this.loginForm.valid) {
+      console.log('Formulário enviado', this.loginForm.value);
+    } else {
+      alert('Verifique os campos email e senha');
+    }
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+}
---
+++ frontend/src/app/login/login.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from '../components/pages/login/login.component';
+import { CadastroComponent } from '../components/pages/cadastro/cadastro.component';
+import { HomeComponent } from '../components/pages/home/home.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    CadastroComponent,
+    HomeComponent,
+    HeaderComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule, // Importando ReactiveFormsModule
+  ],
+  exports: [HeaderComponent],
+})
+export class LoginModule {}
