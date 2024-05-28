fda077bf84f64603a6270dfb1fe49c9f358b325a
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2024-05-28 08:34:17 -0300

    alt: página cadastro

--- frontend/src/app/components/pages/cadastro/cadastro.component.ts
+++ frontend/src/app/components/pages/cadastro/cadastro.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrl: './cadastro.component.css',
 })
 export class CadastroComponent {
-
+  tipoCadastro: string = '';
 }
