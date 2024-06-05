abdb5c244b73e4aab3a1727a2f400ddefe2294ba
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2024-06-04 22:06:57 -0300

    ok

--- frontend/src/app/components/pages/cadastro/cadastro.component.ts
+++ frontend/src/app/components/pages/cadastro/cadastro.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css',
+  styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent {
+  cadastroForm: FormGroup;
   tipoCadastro: string = '';
+
+  constructor(private fb: FormBuilder) {
+    this.cadastroForm = this.fb.group({
+      email: [''],
+      password: [''],
+      tipoUsuario: [''], // Adicionado controle para tipoUsuario
+    });
+  }
+
+  onSubmit(event: Event): void {
+    event.preventDefault(); // Evita o comportamento padrão de envio do formulário
+    alert('Funciona ');
+  }
+
+  updateTipoCadastro(tipo: string): void {
+    this.tipoCadastro = tipo;
+  }
 }
