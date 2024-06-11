d872c791f06e75c756ba0b63a178a76c93127e69
alinnecardoso <dev14cardoso@gmail.com>
2024-06-11 11:55:23 -0300

    Novo Componente "comofunciona"

---
+++ frontend/src/app/components/comofunciona/comofunciona.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comofunciona',
+  templateUrl: './comofunciona.component.html',
+  styleUrl: './comofunciona.component.css'
+})
+export class ComofuncionaComponent {
+
+}
--- frontend/src/app/login/login.module.ts
+++ frontend/src/app/login/login.module.ts
@@ -6,6 +6,7 @@ import { CadastroComponent } from '../components/pages/cadastro/cadastro.compone
 import { HomeComponent } from '../components/pages/home/home.component';
 import { HeaderComponent } from '../components/header/header.component';
 import { RouterModule } from '@angular/router';
+import { ComofuncionaComponent } from '../components/comofunciona/comofunciona.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { RouterModule } from '@angular/router';
     CadastroComponent,
     HomeComponent,
     HeaderComponent,
+    ComofuncionaComponent,
   ],
   imports: [
     CommonModule,
