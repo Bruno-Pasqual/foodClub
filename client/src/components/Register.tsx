24ef3127cd689bfc5a13b976fb75b769095ea1ee
Alinne <alinne14cardoso@gmail.com>
2024-10-04 16:23:44 -0300

    Atualizando a parte de Cadastro do restaurante, todas as informações são armazenadas no Obj e logada no console.

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -10,76 +10,137 @@ import GenericInput from "./GenericInput";
 import { FormEvent, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Register.css";
+import RestaurantRegister from "./RestaurantRegister";
+
+interface FormData {
+  email: string;
+  password1: string;
+  password2: string;
+  role: string;
+}
+
+
 
 const Register = () => {
-	const [step, setSteps] = useState<number>(0);
+
+	const [ step, setStep ] = useState<number>(1);
+	const [ role, setRole ] = useState<string>('restaurante');
+	const [ data, setData ] = useState({}); // novo estado para armazenar os dados do formulário
+
+
+	function handleStep(){
+		if(step === 1){
+			setStep(step + 1);
+		}
+		else if(step <= 2){
+			setStep(step - 1);
+		}
+	}
 
 	function handleSubmit(event: FormEvent<HTMLFormElement>): void {
 		event.preventDefault();
 
 		const formData = new FormData(event.currentTarget);
 
-		const data = {
-			email: formData.get("email"),
-			password1: formData.get("password1"),
-			password2: formData.get("password2"),
-			role: formData.get("row-radio-buttons-group"),
+		const newData: FormData = {
+			email: formData.get("email") as string,
+			password1: formData.get("password1") as string,
+			password2: formData.get("password2") as string,
+			role: formData.get("row-radio-buttons-group") as string,
 		};
+		
+		setData(newData); // armazena os dados do formulário no estado
+		handleStep();
+	}
 
-		console.log(data);
+	function  handleRoleChange(event: React.ChangeEvent<HTMLInputElement>) {
+		setRole(event.target.value);
 	}
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<div className="basic-info-container">
-				<h1>Vamos criar sua conta</h1>
-				<GenericInput
-					type="email"
-					placeholder="Ex: sara@gmail.com"
-					labelText="Email"
-					name="email"
-				/>
-				<GenericInput
-					type="password"
-					placeholder="Digite a sua senha"
-					labelText="Digite a sua senha"
-					name="password1"
-				/>
-				<GenericInput
-					type="password"
-					placeholder="Digite a sua senha novamente"
-					labelText="Confirme a sua senha"
-					name="password2"
-				/>
-				<div>
-					<div>
-						<FormControl>
-							<FormLabel id="demo-row-radio-buttons-group-label">
-								Você quer se cadastrar como
-							</FormLabel>
-							<RadioGroup
-								row
-								aria-labelledby="demo-row-radio-buttons-group-label"
-								name="row-radio-buttons-group"
-							>
-								<FormControlLabel
-									value="restaurante"
-									control={<Radio />}
-									label="Restaurante"
-								/>
-								<FormControlLabel value="empresa" control={<Radio />} label="Empresa" />
-							</RadioGroup>
-						</FormControl>
-					</div>
-				</div>
-				<Button variant="contained" color="primary" type="submit">
-					Continuar
-				</Button>
-			</div>
-			<span>
-				Já tem uma conta? <NavLink to={"/login"}> Entrar</NavLink>
-			</span>
-		</form>
+		<>
+			{ step === 1 && (
+				<form onSubmit={handleSubmit}>
+						<div className="basic-info-container">
+							<h1>Vamos criar sua conta</h1>
+							<GenericInput
+								type="email"
+								placeholder="Ex: sara@gmail.com"
+								labelText="Email"
+								name="email"
+							/>
+							<GenericInput
+								type="password"
+								placeholder="Digite a sua senha"
+								labelText="Digite a sua senha"
+								name="password1"
+							/>
+							<GenericInput
+								type="password"
+								placeholder="Digite a sua senha novamente"
+								labelText="Confirme a sua senha"
+								name="password2"
+							/>
+							<div>
+								<div>
+									<FormControl>
+										<FormLabel id="demo-row-radio-buttons-group-label">
+											Você quer se cadastrar como
+										</FormLabel>
+										<RadioGroup
+											row
+											aria-labelledby="demo-row-radio-buttons-group-label"
+											name="row-radio-buttons-group"
+											onChange={handleRoleChange}
+										>
+											<FormControlLabel
+												value="restaurante"
+												control={<Radio />}
+												label="Restaurante"
+											/>
+											<FormControlLabel value="empresa" control={<Radio />} label="Empresa" />
+										</RadioGroup>
+									</FormControl>
+								</div>
+							</div>
+							<Button variant="contained" color="primary"type="submit" >
+								Continuar
+							</Button>
+						</div>
+						<span>
+						Já tem uma conta? <NavLink to={"/login"}> Entrar</NavLink>
+						</span>
+				</form>
+			) }
+
+			{ step === 2 && role === 'restaurante' &&(
+				<>
+					<Button  onClick={handleStep} >
+					Voltar
+					</Button>
+
+					< RestaurantRegister  data={data} />
+
+					
+					
+				</>
+			)}
+
+			{ step === 2 && role === 'empresa' &&(
+				<>
+					<Button  onClick={handleStep} >
+					Voltar
+					</Button>
+					
+					<h1>Empresa</h1>
+					<form onSubmit={handleSubmit} >
+						<Button variant="contained" color="primary" type="submit">
+							Registrar
+						</Button>
+        	</form>
+				</>
+			)}
+		</>
 	);
 };
 
---
+++ client/src/components/RestaurantRegister.tsx
@@ -0,0 +1,64 @@
+import { FormEvent } from "react";
+import GenericInput from "./GenericInput";
+import { Button } from "@mui/material";
+
+// Definindo a interface para as props do componente
+interface RestaurantRegisterProps {
+  data: object; // Use o tipo FormData para data
+}
+
+const RestaurantRegister = ({data}:RestaurantRegisterProps) =>{
+  function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+		event.preventDefault();
+
+		const formData = new FormData(event.currentTarget);
+
+		const restaurantData = {
+      ...data,
+			name: formData.get("name"),
+			cnpj: formData.get("cnpj"),
+			cep: formData.get("cep"),
+			number: formData.get("number"),
+		};
+		console.log('Restaurant -> Button Clicked')
+		console.log(restaurantData);
+	}
+  return (
+    <>
+    
+    <form onSubmit={handleSubmit} >
+      <h1>Restaurante</h1>
+      <GenericInput
+        type="text"
+        placeholder="Nome do seu restaurante"
+        labelText="Nome do seu restaurante"
+        name="name"
+      />
+      <GenericInput
+        type="text"
+        placeholder="CNPJ"
+        labelText="Digite o CNPJ"
+        name="cnpj"
+      />
+      <GenericInput
+        type="text"
+        placeholder="Digite o CEP"
+        labelText="CEP"
+        name="cep"
+      />
+      <GenericInput
+        type="number"
+        placeholder="Número"
+        labelText="Número"
+        name="number"
+      />
+
+      <Button variant="contained" color="primary"type="submit" >
+								Registrar
+      </Button>
+    </form>
+  </>
+  )
+}
+
+export default  RestaurantRegister;
