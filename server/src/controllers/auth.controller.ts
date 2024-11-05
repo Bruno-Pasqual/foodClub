2a7253bb8537c01863bd135c80b15fb99ddee8c4
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2024-11-04 22:27:50 -0300

    feat: signup validations

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,17 +1,6 @@
 //#region Imports
-
-import bcrypt from "bcrypt";
 import { Request, RequestHandler, Response } from "express";
 
-import { UserType } from "../models/enums/enums";
-import { Company } from "../models/Company";
-import { Employee } from "../models/Employee";
-import { Restaurant } from "../models/Restaurant";
-import {
-	getErrorCreationMessage,
-	getCreationMessage as getSuccessCreationMessage,
-} from "../utils/helpers";
-import { validSignupFields } from "../utils/validations";
 import {
 	ICompany,
 	IEmployee,
@@ -19,63 +8,40 @@ import {
 	IValidations as IFieldsValidationsDTO,
 	IUser,
 } from "../models/interfaces/interfaces";
-import { User } from "../models/User";
 
 //#endregion
 
 export const signup = async (req: Request, res: Response): Promise<any> => {
 	const userData: IRestaurant | IEmployee | ICompany = req.body;
 
-	const userDataIsEmpty = (obj: object): boolean => {
-		return Object.keys(obj).length === 0;
-	};
+	const values = Object.entries(userData).map(([key, value]) => {
+		return { [key]: value };
+	});
 
-	if (userDataIsEmpty(userData)) {
-		return res
-			.status(400)
-			.json({ success: false, message: "User data is empty.", data: null });
+	console.log(values);
+
+	const emptyValue = values.some((value) => {
+		return value.value === undefined;
+	});
+
+	console.log("emptyValue", emptyValue);
+
+	if (emptyValue) {
+		return res.status(400).json({
+			message: "Todos os campos obrigatórios devem ser preenchidos",
+			code: 400,
+		});
 	}
 
-	const validFields = await validSignupFields(
-		userData as IFieldsValidationsDTO,
-		res
-	);
+	switch (userData.userType) {
+		case "company":
+		case "employee":
+		case "restaurant":
 
-	console.log(validFields);
-
-	if (validFields) {
-		try {
-			userData.password = await bcrypt.hash(userData.password, 10);
-
-			let newUser: IRestaurant | IEmployee | ICompany;
-
-			switch (userData.userType) {
-				case UserType.COMPANY:
-					newUser = await Company.create(userData);
-					break;
-				case UserType.RESTAURANT:
-					newUser = await Restaurant.create(userData);
-					break;
-				case UserType.EMPLOYEE:
-					newUser = await Employee.create(userData);
-					break;
-				default:
-					return res
-						.status(400)
-						.json({ success: false, message: "User type is invalid.", data: null });
-			}
-
-			return res.status(201).json({
-				success: true,
-				message: getSuccessCreationMessage(userData.userType),
-				user: { ...newUser, password: undefined },
+		default:
+			return res.status(400).json({
+				message: "Tipo de usuário inválido",
+				code: 400,
 			});
-		} catch (error) {
-			console.log(error);
-			return res.status(500).json({
-				success: false,
-				message: error,
-			});
-		}
 	}
 };
--- server/src/utils/validations.ts
+++ server/src/utils/validations.ts
@@ -1,199 +1,199 @@
-import { Response } from "express";
-import { IValidations } from "../models/interfaces/interfaces";
-import { User } from "./../models/User";
-import { ErrorMessages, UserType } from "../models/enums/enums";
+// import { Response } from "express";
+// import { IValidations } from "../models/interfaces/interfaces";
+// import { User } from "./../models/User";
+// import { ErrorMessages, UserType } from "../models/enums/enums";
 
-//#region Common
+// //#region Common
 
-export const validateEmail = async (
-	email: string | undefined,
-	res: Response
-) => {
-	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Valida um email com formato: algo@dominio.ext
+// export const validateEmail = async (
+// 	email: string | undefined,
+// 	res: Response
+// ) => {
+// 	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Valida um email com formato: algo@dominio.ext
 
-	if (!email) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.EMPTY_EMAIL });
-	}
+// 	if (!email) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.EMPTY_EMAIL });
+// 	}
 
-	if (!regex.test(email)) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.EMAIL_NOT_VALID });
-	}
+// 	if (!regex.test(email)) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.EMAIL_NOT_VALID });
+// 	}
 
-	const UserExist = await User.findOne({ email: email });
+// 	const UserExist = await User.findOne({ email: email });
 
-	if (UserExist) {
-		return res.status(409).json({
-			success: false,
-			message: ErrorMessages.EMAIL_ALREADY_USED,
-		});
-	}
-};
+// 	if (UserExist) {
+// 		return res.status(409).json({
+// 			success: false,
+// 			message: ErrorMessages.EMAIL_ALREADY_USED,
+// 		});
+// 	}
+// };
 
-export const validatePassword = (
-	password: string | undefined,
-	res: Response
-) => {
-	const regex =
-		/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+// export const validatePassword = (
+// 	password: string | undefined,
+// 	res: Response
+// ) => {
+// 	const regex =
+// 		/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-	if (!password) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.EMPTY_PASSWORD });
-	}
+// 	if (!password) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.EMPTY_PASSWORD });
+// 	}
 
-	if (!regex.test(password)) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.INVALID_PASSWORD });
-	}
-};
+// 	if (!regex.test(password)) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.INVALID_PASSWORD });
+// 	}
+// };
 
-export const validateName = (name: string | undefined, res: Response) => {
-	console.log("entrei");
+// export const validateName = (name: string | undefined, res: Response) => {
+// 	console.log("entrei");
 
-	if (!name) {
-		return res.status(400).json({
-			success: false,
-			message: ErrorMessages.EMPTY_NAME,
-		});
-	}
-	if (name.length < 3) {
-		return res.status(400).json({
-			success: false,
-			message: ErrorMessages.TOO_SHORT_NAME,
-		});
-	}
-};
+// 	if (!name) {
+// 		return res.status(400).json({
+// 			success: false,
+// 			message: ErrorMessages.EMPTY_NAME,
+// 		});
+// 	}
+// 	if (name.length < 3) {
+// 		return res.status(400).json({
+// 			success: false,
+// 			message: ErrorMessages.TOO_SHORT_NAME,
+// 		});
+// 	}
+// };
 
-//#endregion
+// //#endregion
 
-//#region Restaurant / Company
+// //#region Restaurant / Company
 
-//#endregion
+// //#endregion
 
-export const validateCpf = (cpf: string | undefined, res: Response) => {
-	if (!cpf) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.EMPTY_CPF });
-	}
+// export const validateCpf = (cpf: string | undefined, res: Response) => {
+// 	if (!cpf) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.EMPTY_CPF });
+// 	}
 
-	if (cpf.length !== 11 || isNaN(Number(cpf))) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.INVALID_CPF });
-	}
-};
+// 	if (cpf.length !== 11 || isNaN(Number(cpf))) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.INVALID_CPF });
+// 	}
+// };
 
-export const validateCep = (cep: string | undefined, res: Response) => {
-	if (!cep) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.EMPTY_CEP });
-	}
+// export const validateCep = (cep: string | undefined, res: Response) => {
+// 	if (!cep) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.EMPTY_CEP });
+// 	}
 
-	if (cep.length !== 8 || isNaN(Number(cep))) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.INVALID_CEP });
-	}
-};
+// 	if (cep.length !== 8 || isNaN(Number(cep))) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.INVALID_CEP });
+// 	}
+// };
 
-export const validatePrice = (price: number | undefined, res: Response) => {
-	if (!price) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.EMPTY_PRICE });
-	}
+// export const validatePrice = (price: number | undefined, res: Response) => {
+// 	if (!price) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.EMPTY_PRICE });
+// 	}
 
-	if (price < 0) {
-		return res.status(400).json({
-			success: false,
-			message: ErrorMessages.INVALID_PRICE,
-		});
-	}
-};
+// 	if (price < 0) {
+// 		return res.status(400).json({
+// 			success: false,
+// 			message: ErrorMessages.INVALID_PRICE,
+// 		});
+// 	}
+// };
 
-export const validateNumber = (number: number | undefined, res: Response) => {
-	if (!number) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.EMPTY_NUMBER });
-	}
+// export const validateNumber = (number: number | undefined, res: Response) => {
+// 	if (!number) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.EMPTY_NUMBER });
+// 	}
 
-	if (number <= 0) {
-		return res.status(400).json({
-			success: false,
-			message: "Número inválido. Deve ser maior que zero.",
-		});
-	}
-};
+// 	if (number <= 0) {
+// 		return res.status(400).json({
+// 			success: false,
+// 			message: "Número inválido. Deve ser maior que zero.",
+// 		});
+// 	}
+// };
 
-export const validateDescription = (description: string, res: Response) => {
-	if (!description) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.EMPTY_DESCRIPTION });
-	}
+// export const validateDescription = (description: string, res: Response) => {
+// 	if (!description) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.EMPTY_DESCRIPTION });
+// 	}
 
-	if (description.length < 30) {
-		return res.status(400).json({
-			success: false,
-			message: ErrorMessages.EMPTY_DESCRIPTION,
-		});
-	}
-};
+// 	if (description.length < 30) {
+// 		return res.status(400).json({
+// 			success: false,
+// 			message: ErrorMessages.EMPTY_DESCRIPTION,
+// 		});
+// 	}
+// };
 
-export const validateCNPJ = (cnpj: string | undefined, res: Response) => {
-	if (!cnpj) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.EMPTY_CNPJ });
-	}
+// export const validateCNPJ = (cnpj: string | undefined, res: Response) => {
+// 	if (!cnpj) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.EMPTY_CNPJ });
+// 	}
 
-	if (cnpj.length !== 14 || isNaN(Number(cnpj))) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.INVALID_CNPJ });
-	}
+// 	if (cnpj.length !== 14 || isNaN(Number(cnpj))) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.INVALID_CNPJ });
+// 	}
 
-	// Regex para verificar se todos os números são iguais
-	const repeatedNumberPattern = /^(\d)\1{13}$/;
-	if (repeatedNumberPattern.test(cnpj)) {
-		return res
-			.status(400)
-			.json({ success: false, message: ErrorMessages.INVALID_CNPJ });
-	}
+// 	// Regex para verificar se todos os números são iguais
+// 	const repeatedNumberPattern = /^(\d)\1{13}$/;
+// 	if (repeatedNumberPattern.test(cnpj)) {
+// 		return res
+// 			.status(400)
+// 			.json({ success: false, message: ErrorMessages.INVALID_CNPJ });
+// 	}
 
-	// Código adicional para outras validações do CNPJ, se necessário
-};
+// 	// Código adicional para outras validações do CNPJ, se necessário
+// };
 
-export const validSignupFields = async (
-	fields: IValidations,
-	res: Response
-): Promise<boolean> => {
-	//Validações comuns
-	validateName(fields.name, res);
-	validateEmail(fields.email, res);
-	validatePassword(fields.password, res);
+// export const validSignupFields = async (
+// 	fields: IValidations,
+// 	res: Response
+// ): Promise<boolean> => {
+// 	//Validações comuns
+// 	validateName(fields.name, res);
+// 	validateEmail(fields.email, res);
+// 	validatePassword(fields.password, res);
 
-	// employee
-	if (fields.userType === UserType.EMPLOYEE) {
-		validateCpf(fields.cpf, res);
-	}
+// 	// employee
+// 	if (fields.userType === UserType.EMPLOYEE) {
+// 		validateCpf(fields.cpf, res);
+// 	}
 
-	if (
-		fields.userType === UserType.COMPANY ||
-		fields.userType === UserType.RESTAURANT
-	) {
-		validateCep(fields.cep, res);
-		validateNumber(fields.number, res);
-		validateCNPJ(fields.cnpj, res);
-	}
-	return true;
-};
+// 	if (
+// 		fields.userType === UserType.COMPANY ||
+// 		fields.userType === UserType.RESTAURANT
+// 	) {
+// 		validateCep(fields.cep, res);
+// 		validateNumber(fields.number, res);
+// 		validateCNPJ(fields.cnpj, res);
+// 	}
+// 	return true;
+// };
