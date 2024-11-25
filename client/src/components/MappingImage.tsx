2a8100a1f72fd27992e14e5846839f861eb06e51
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2024-11-24 22:28:28 -0300

    feat: frontend deploy

--- client/src/components/MappingImage.tsx
+++ client/src/components/MappingImage.tsx
@@ -1,13 +1,10 @@
-import { employeesFoto } from '../data/mockData'
+// interface IProps {
+// 	image: [object];
+// }
 
-interface IProps {
-    image: [object];
-}
-
-export const MappingImage = ({image}:IProps) => {
-    return (
-        {employeesFoto.map(({image}) => {
-
-        })}
-    )
-}
+// export const MappingImage = ({  }: IProps) => {
+// 	// return (
+// 	//     // {employeesFoto.map(({image}) => {
+// 	//     // })}
+// 	// )
+// };
--- client/src/components/ProgressBar/ProgressBar.tsx
+++
@@ -1,3 +0,0 @@
-const ProgressBar = (props: Props) => {
-	return <div className="progress-bar"></div>;
-};
