bc45ff679b9d889ae08d9dc201eccec50d108ce2
Bruno-Pasqual <brunoh.pasqual0@gmail.com>
2024-11-24 22:37:00 -0300

    fix: cors policy from multiple origins

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,8 +13,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
 	cors({
-		origin: "http://localhost:5173",
-		credentials: true,
+		origin: (origin, callback) => {
+			// Lista de origens permitidas
+			const allowedOrigins = [
+				"http://localhost:5173",
+				"https://foodclub2.netlify.app",
+			];
+
+			// Permitir requisições sem 'origin' (como as feitas por ferramentas locais como Postman)
+			if (!origin || allowedOrigins.includes(origin)) {
+				callback(null, true);
+			} else {
+				callback(new Error("Não permitido pela política de CORS"));
+			}
+		},
+		credentials: true, // Permitir envio de cookies
 	})
 );
 app.use(cookieParser());
